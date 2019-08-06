19edcdf0eb716588de99c90d8db7be8fbcad9185
Seth Lachman <lachmanseth17@gmail.com>
2019-08-06 14:54:46 -0400

    Light background theme.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,26 @@ class Home extends React.Component {
             * {
               font-family: "Nunito Sans", sans-serif;
             }
+
+            html {
+              font-size: 62.5%;
+            }
+
+            body {
+              background: hsl(0, 0%, 98%);
+              color: hsl(200, 15%, 8%);
+            }
+
+            h1,
+            h2,
+            h3 {
+              margin: 0;
+            }
+
+            p,
+            li {
+              font-size: 1.4rem;
+            }
           `}
         />
         <main
@@ -36,33 +56,78 @@ class Home extends React.Component {
             css={css`
               display: grid;
               grid-template-columns: repeat(4, 1fr);
-              grid-gap: 20px;
+              grid-gap: 2rem;
             `}
           >
             {countries &&
               countries.map(country => {
                 console.log(country);
                 return (
-                  <div key={country.name}>
+                  <div
+                    css={css`
+                      border-radius: 5px;
+                      box-shadow: 0 3px 4px rgba(0, 0, 0, 0.3);
+                      background: #fefefe;
+                    `}
+                    key={country.name}
+                  >
                     <img
                       src={country.flag}
                       alt={country.name}
                       css={css`
                         max-width: 100%;
-                        height: auto;
+                        width: 285px;
+                        height: 170px;
                       `}
                     />
-                    <div>
-                      <h3>{country.name}</h3>
+                    <div
+                      css={css`
+                        padding: 2rem;
+                      `}
+                    >
+                      <h3
+                        css={css`
+                          font-size: 1.6rem;
+                        `}
+                      >
+                        {country.name}
+                      </h3>
                       <ul
                         css={css`
                           padding-left: 0;
                           list-style: none;
                         `}
                       >
-                        <li>Population: {country.population}</li>
-                        <li>Region: {country.region}</li>
-                        <li>Capital: {country.capital}</li>
+                        <li>
+                          <span
+                            css={css`
+                              font-weight: 800;
+                            `}
+                          >
+                            Population:
+                          </span>{" "}
+                          {country.population}
+                        </li>
+                        <li>
+                          <span
+                            css={css`
+                              font-weight: 800;
+                            `}
+                          >
+                            Region:
+                          </span>{" "}
+                          {country.region}
+                        </li>
+                        <li>
+                          <span
+                            css={css`
+                              font-weight: 800;
+                            `}
+                          >
+                            Capital:
+                          </span>{" "}
+                          {country.capital}
+                        </li>
                       </ul>
                     </div>
                   </div>
