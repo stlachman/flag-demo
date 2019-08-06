3aa0cc05b71e4b5df0e42783fd856cfc5d6c052a
Seth Lachman <lachmanseth17@gmail.com>
2019-08-06 13:31:15 -0400

    Added initial base styles.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { css } from "@emotion/core";
+import { Global, css } from "@emotion/core";
 import axios from "axios";
 
 class Home extends React.Component {
@@ -18,37 +18,59 @@ class Home extends React.Component {
       );
     }
     return (
-      <main
-        css={css`
-          max-width: 1200px;
-          margin: 0 auto;
-        `}
-      >
-        <section
+      <>
+        <Global
+          styles={css`
+            * {
+              font-family: "Nunito Sans", sans-serif;
+            }
+          `}
+        />
+        <main
           css={css`
-            display: grid;
-            grid-template-columns: repeat(4, 1fr);
-            grid-gap: 20px;
+            max-width: 1200px;
+            margin: 0 auto;
           `}
         >
-          {countries &&
-            countries.map(country => {
-              console.log(country);
-              return (
-                <div key={country.name}>
-                  <img
-                    src={country.flag}
-                    alt={country.name}
-                    css={css`
-                      max-width: 100%;
-                      height: auto;
-                    `}
-                  />
-                </div>
-              );
-            })}
-        </section>
-      </main>
+          <section
+            css={css`
+              display: grid;
+              grid-template-columns: repeat(4, 1fr);
+              grid-gap: 20px;
+            `}
+          >
+            {countries &&
+              countries.map(country => {
+                console.log(country);
+                return (
+                  <div key={country.name}>
+                    <img
+                      src={country.flag}
+                      alt={country.name}
+                      css={css`
+                        max-width: 100%;
+                        height: auto;
+                      `}
+                    />
+                    <div>
+                      <h3>{country.name}</h3>
+                      <ul
+                        css={css`
+                          padding-left: 0;
+                          list-style: none;
+                        `}
+                      >
+                        <li>Population: {country.population}</li>
+                        <li>Region: {country.region}</li>
+                        <li>Capital: {country.capital}</li>
+                      </ul>
+                    </div>
+                  </div>
+                );
+              })}
+          </section>
+        </main>
+      </>
     );
   }
 
