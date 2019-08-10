3ad92ea0ad09c80c43e8b009b40106c129560623
Seth Lachman <lachmanseth17@gmail.com>
2019-08-10 13:37:33 -0400

    Updated styles on single country page.

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -10,12 +10,13 @@ class Country extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      country: []
+      country: [],
+      borders: []
     };
   }
   componentDidMount() {
-    const id = this.props.match.params.id;
-    this.fetchCountry(id);
+    const name = this.props.match.params.id;
+    this.fetchCountry(name);
   }
 
   fetchCountry = name => {
@@ -29,6 +30,17 @@ class Country extends React.Component {
       .catch(console.error);
   };
 
+  // convertBorder = inititals => {
+  //   axios
+  //     .get(`https://restcountries.eu/rest/v2/alpha/${inititals}`)
+  //     .then(res =>
+  //       this.setState({
+  //         borders: [this.state.borders, ...res.data.name]
+  //       })
+  //     )
+  //     .catch(console.error);
+  // };
+
   render() {
     const { country } = this.state;
     const renderMultipleItems = arr => {
@@ -38,7 +50,7 @@ class Country extends React.Component {
     const renderObj = (arr, key) => {
       return arr[0][key];
     };
-    console.log(country);
+    // console.log(country);
     if (country.length === 0) {
       return (
         <>
@@ -70,34 +82,61 @@ class Country extends React.Component {
         >
           <div
             css={css`
-              display: grid;
-              grid-template-columns: repeat(2, 1fr);
-              grid-gap: 20px;
+              display: flex;
+              flex-wrap: wrap;
             `}
           >
             <div>
-              <Link to="/">Back</Link>
+              <Link
+                to="/"
+                css={css`
+                  padding: 1rem 2rem;
+                  font-weight: 600;
+                  font-size: 1.6rem;
+                  text-decoration: none;
+                  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.25);
+                  margin: 0 0 1rem;
+                  display: inline-block;
+                `}
+              >
+                Back
+              </Link>
 
               <img
                 css={css`
                   max-width: 100%;
                   height: auto;
+                  display: block;
                 `}
                 src={country.flag}
                 alt={country.name}
               />
             </div>
 
-            <div>
-              <h2>{country.name}</h2>
-
-              <div>
+            <div
+              css={css`
+                display: grid;
+                grid-template-columns: repeat(2, 1fr);
+                grid-template-rows: 3fr 1fr;
+                margin-left: 10%;
+              `}
+            >
+              <div
+                css={css`
+                  align-self: center;
+                `}
+              >
+                <h2>{country.name}</h2>
                 <p>Native Name: {country.nativeName}</p>
                 <p>Population: {country.population}</p>
                 <p>Region: {country.region}</p>
                 <p>Capital: {country.capital}</p>
               </div>
-              <div>
+              <div
+                css={css`
+                  align-self: center;
+                `}
+              >
                 <p>
                   Top Level Domain:{" "}
                   {renderMultipleItems(country.topLevelDomain)}
@@ -105,11 +144,26 @@ class Country extends React.Component {
                 <p>Currencies: {renderObj(country.currencies, "name")}</p>
                 <p>Languages: {renderObj(country.languages, "name")}</p>
               </div>
-              <div>
+              <div
+                css={css`
+                  grid-column: 1 / 3;
+                `}
+              >
                 <p>
                   Border Countries:{" "}
                   {country.borders.map(border => (
-                    <span key={border}>{border}</span>
+                    <span
+                      css={css`
+                        margin: 0 1.5rem;
+                        padding: 1rem 2rem;
+                        font-weight: 600;
+                        font-size: 1.4rem;
+                        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.25);
+                      `}
+                      key={border}
+                    >
+                      {this.convertBorder(border)}
+                    </span>
                   ))}
                 </p>
               </div>
