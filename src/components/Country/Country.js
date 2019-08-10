1f06c3af9315417eb0e9074b9558604357effbf8
Seth Lachman <lachmanseth17@gmail.com>
2019-08-10 11:55:02 -0400

    All data for country displayed.

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -20,14 +20,24 @@ class Country extends React.Component {
 
   fetchCountry = name => {
     axios
-      .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .get(
+        `https://restcountries.eu/rest/v2/name/${name}?fields=name;flag;nativeName;population;region;subregion;capital;topLevelDomain;currencies;languages;borders`
+      )
       .then(res => {
         this.setState({ country: res.data[0] });
       })
       .catch(console.error);
   };
+
   render() {
     const { country } = this.state;
+    const renderMultipleItems = arr => {
+      return arr.length > 1 ? arr.map(item => item + ",") : arr[0];
+    };
+
+    const renderObj = (arr, key) => {
+      return arr[0][key];
+    };
     console.log(country);
     if (country.length === 0) {
       return (
@@ -58,14 +68,52 @@ class Country extends React.Component {
             margin: 7rem auto 0;
           `}
         >
-          <div>
-            <Link to="/">Back</Link>
+          <div
+            css={css`
+              display: grid;
+              grid-template-columns: repeat(2, 1fr);
+              grid-gap: 20px;
+            `}
+          >
+            <div>
+              <Link to="/">Back</Link>
 
-            <img src={country.flag} alt={country.name} />
-          </div>
+              <img
+                css={css`
+                  max-width: 100%;
+                  height: auto;
+                `}
+                src={country.flag}
+                alt={country.name}
+              />
+            </div>
 
-          <div>
-            <h2>{this.state.country.name}</h2>
+            <div>
+              <h2>{country.name}</h2>
+
+              <div>
+                <p>Native Name: {country.nativeName}</p>
+                <p>Population: {country.population}</p>
+                <p>Region: {country.region}</p>
+                <p>Capital: {country.capital}</p>
+              </div>
+              <div>
+                <p>
+                  Top Level Domain:{" "}
+                  {renderMultipleItems(country.topLevelDomain)}
+                </p>
+                <p>Currencies: {renderObj(country.currencies, "name")}</p>
+                <p>Languages: {renderObj(country.languages, "name")}</p>
+              </div>
+              <div>
+                <p>
+                  Border Countries:{" "}
+                  {country.borders.map(border => (
+                    <span key={border}>{border}</span>
+                  ))}
+                </p>
+              </div>
+            </div>
           </div>
         </main>
       </>
